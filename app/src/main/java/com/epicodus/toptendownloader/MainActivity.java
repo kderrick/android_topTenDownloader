70b47c155f3aea743399c9b7a323622cd134e527
Kyle <kderrick1988@gmail.com>
2016-06-14 13:30:57 -0700

    add initial files

---
+++ app/src/main/java/com/epicodus/toptendownloader/MainActivity.java
@@ -0,0 +1,18 @@
+package com.epicodus.toptendownloader;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+
+    }
+}
