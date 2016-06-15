d50edcc48db1d31df6f093181bfd63e8b00b6760
Kyle <kderrick1988@gmail.com>
2016-06-14 19:54:09 -0700

    add override of doInBackground

--- app/src/main/java/com/epicodus/toptendownloader/MainActivity.java
+++ app/src/main/java/com/epicodus/toptendownloader/MainActivity.java
@@ -1,8 +1,16 @@
 package com.epicodus.toptendownloader;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +22,40 @@ public class MainActivity extends AppCompatActivity {
 
     private class DownloadData extends AsyncTask<String, Void, String> {
 
+        private String mFileContents;
+
+        @Override
+        protected String doInBackground(String... params) {
+            mFileContents = downloadXMLFile(params[0]);
+            if(mFileContents == null) {
+                Log.d("DownloadData" , "Error downloading");
+            }
+            return mFileContents;
+        }
+        private String downloadXMLFile(String urlPath) {
+            StringBuilder tempBuffer = new StringBuilder();
+            try {
+                URL url = new URL(urlPath);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                int response = connection.getResponseCode();
+                Log.d("DownloadData", "The response code was" + response);
+                InputStream is = connection.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+
+                int charRead;
+                char[] inputBuffer = new char[500];
+                while(true) {
+                    charRead = isr.read(inputBuffer);
+                    if(charRead <= 0 ) {
+                        break;
+                    }
+                    tempBuffer.append(String.copyValueOf(inputBuffer, 0, charRead));
+                }
+                return tempBuffer.toString();
+
+            } catch (IOException e) {
+                Log.d("DownloadData", "IOException reading data" + e.getMessage());
+            }
+        }
     }
 }
