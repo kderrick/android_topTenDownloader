339d34cc3374906654413456b1aeaa6ee26a477c
Kyle <kderrick1988@gmail.com>
2016-06-14 20:20:36 -0700

    add downloadXMLFile method

--- app/src/main/java/com/epicodus/toptendownloader/MainActivity.java
+++ app/src/main/java/com/epicodus/toptendownloader/MainActivity.java
@@ -10,7 +10,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
     }
 
     private class DownloadData extends AsyncTask<String, Void, String> {
@@ -32,13 +33,20 @@ public class MainActivity extends AppCompatActivity {
             }
             return mFileContents;
         }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            Log.d("DownloadData", "Result was" + result);
+        }
+
         private String downloadXMLFile(String urlPath) {
             StringBuilder tempBuffer = new StringBuilder();
             try {
                 URL url = new URL(urlPath);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
                 int response = connection.getResponseCode();
-                Log.d("DownloadData", "The response code was" + response);
+                Log.d("DownloadData", "The response code was " + response);
                 InputStream is = connection.getInputStream();
                 InputStreamReader isr = new InputStreamReader(is);
 
@@ -55,7 +63,11 @@ public class MainActivity extends AppCompatActivity {
 
             } catch (IOException e) {
                 Log.d("DownloadData", "IOException reading data" + e.getMessage());
+//                e.printStackTrace();
+            } catch (SecurityException e) {
+                Log.d("DownloadData", "Security Exception. Need permissions? " + e.getMessage());
             }
+            return null;
         }
     }
 }
